ff0761e3a7a1f44c25b86680cd59bba29d27cb8e
kotsos2000 <evilkotsos@gmail.com>
2023-01-26 00:50:22 +0200

    /

--- tests/init.test.js
+++ tests/init.test.js
@@ -9,6 +9,7 @@ const listen = require('test-listen');
 
 const app = require('../src/index');
 const {jwtSign} = require('../src/utilities/authentication/helpers');
+const { string } = require('yup');
 // before each , opens the server
 test.before(async (t) => {
   t.context.server = http.createServer(app);
@@ -23,7 +24,7 @@ test.after.always((t) => {
 test('GET /statistics returns correct response and status code', async (t) => {
   // does a GET request to the path
   const {body, statusCode} = await t.context.got('general/statistics');
-  t.is(body.sources, 2);
+  t.is(body.sources, 1);
   // this test should always pass
   t.assert(body.success);
   t.is(statusCode, 200);
@@ -50,6 +51,34 @@ test('POST /create-source creates a new source', async (t) => {
     name: 'leader', type: 'mqtt', url: 'https://snap.fan/cards/Leader/', login: 'kotsos', passcode: 'kotsos2'
   };
   // return the body , give my token in path and the json as the second input
-  const {body} = await t.context.got.post(`sources/create-source?token=${token}`, {json: newSource});
+  const {statusCode} = await t.context.got.post(`sources/create-source?token=${token}`, {json: newSource});
+  t.is(statusCode, 200);
+});
+
+test('POST /create-source creates the same source as before', async (t) => {
+  // give my id
+  const token = jwtSign({id: '63bdd8ed050a9611142d34c4'});
+  // create a json to give as input to the POST request
+  const newSource = {
+    name: 'leader', type: 'mqtt', url: 'https://snap.fan/cards/Leader/', login: 'kotsos', passcode: 'kotsos2'
+  };
+  // return the body , give my token in path and the json as the second input
+  const {statusCode, body} = await t.context.got.post(`sources/create-source?token=${token}`, {json: newSource});
+  t.is(statusCode, 200);
+  // since its a duplicate we expect the response of the post to be 409
   t.is(body.status, 409);
 });
+
+// test('POST /change-source we change the name of the previous json', async (t) => {
+//   const token = jwtSign({id: '63bdd8ed050a9611142d34c4'});
+//   // get all the sources in order to read the ID and name
+//   const {body} = await t.context.got(`sources/sources?token=${token}`);
+//   const newId = body.sources[1].id;
+//   const oldname = body.sources[1].name;
+//   const changedSource = {
+//     _id: newId, name: oldname, type: 'mqtt', url: 'https://marvelsnapzone.com/decks/zabucula-2/', login: 'kotsos', passcode: 'kotsos2'
+//   };
+//   console.log(changedSource);
+//   const {statusCode} = await t.context.got.post(`sources/change-source?token=${token}`, {json: changedSource});
+//   t.is(statusCode, 200);
+// });
