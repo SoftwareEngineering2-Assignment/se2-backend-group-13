648a3147635460f034316578370aab27a43f373e
kotsos2000 <evilkotsos@gmail.com>
2023-02-01 23:25:15 +0200

    .

--- src/index.js
+++ src/index.js
@@ -1,25 +1,36 @@
 // imports
+// path helps with directories
 const path = require('path');
 require('dotenv').config({path: path.join(__dirname, '../', '.env')});
+// express helps create an app
 const express = require('express');
+// logger middleware
 const logger = require('morgan');
+// parsing middleware
 const bodyParser = require('body-parser');
+// headers
 const helmet = require('helmet');
+// compression middleware
 const compression = require('compression');
+// connect/express
 const cors = require('cors');
+// the module we created
 const {error} = require('./middlewares');
 const routes = require('./routes');
 const {mongoose} = require('./config');
 // express initialization
 const app = express();
+// apply the imported modules
 app.use(cors());
 app.use(helmet());
 // App configuration
 app.use(compression());
+
 // development mode if not test mode
 if (process.env.NODE_ENV !== 'test') {
   app.use(logger('dev'));
 }
+
 // set a limit of 50mb to the request bodies
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: false}));
@@ -35,11 +46,14 @@ app.use(express.static(path.join(__dirname, 'assets')));
 
 // error handler
 app.use(error);
+
 // if the PORT is not given in .env file assume 3000
 const port = process.env.PORT || 3000;
+
 app.listen(port, () =>
 // output to console everytime the server starts
 // eslint-disable-next-line no-console
   console.log(`NodeJS Server listening on port ${port}. \nMode: ${process.env.NODE_ENV}`));
+  
 // end by exporting app
 module.exports = app;
--- src/middlewares/error.js
+++ src/middlewares/error.js
@@ -1,20 +1,25 @@
 /* eslint-disable no-console */
 const {pipe, has, ifElse, assoc, identity, allPass, propEq} = require('ramda');
-
+// everything with status 500 gets sent here in production environment
 const withFormatMessageForProduction = ifElse(
   allPass([propEq('status', 500), () => process.env.NODE_ENV === 'production']),
+  // send message server error
   assoc('message', 'Internal server error occurred.'),
   identity
 );
-
-module.exports = (error, req, res) => 
+// remove req
+module.exports = (error, res) => 
   /**
      * @name error
      * @description Middleware that handles errors
      */
   pipe(
+    // give an incoming error the attribute message
     (e) => ({...e, message: e.message}),
+    // gives 500 if it doesnt already have it
     ifElse(has('status'), identity, assoc('status', 500)),
+    // give the message created previously
     withFormatMessageForProduction,
+    // return the modified error
     (fError) => res.status(fError.status).json(fError)
   )(error);
