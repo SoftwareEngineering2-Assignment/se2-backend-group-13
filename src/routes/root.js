92818cb4fdc0f0400fff6c414e0920e842019990
kotsos2000 <106701436+kotsos2000@users.noreply.github.com>
2023-02-10 10:15:44 +0200

    Update root.js

--- src/routes/root.js
+++ src/routes/root.js
@@ -1,11 +1,13 @@
+// import needed modules
 const express = require('express');
 const path = require('path');
-
+// initialize router
 const router = express.Router();
-
+// read file
 const file = path.join(__dirname, '../../index.html');
+// use file with router
 router.use(express.static(file));
-
+// request handler
 router.get('/', (req, res) => res.sendFile(file));
-
+// export router
 module.exports = router;
