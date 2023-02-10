52aebb90337bb1b883d6c4c9953345376695dfb6
kotsos2000 <106701436+kotsos2000@users.noreply.github.com>
2023-02-10 10:14:20 +0200

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,16 +1,17 @@
+// import needed modules
 const express = require('express');
 const users = require('./users');
 const sources = require('./sources');
 const dashboards = require('./dashboards');
 const general = require('./general');
 const root = require('./root');
-
+// initialize router
 const router = express.Router();
-
+// use the imported modules
 router.use('/users', users);
 router.use('/sources', sources);
 router.use('/dashboards', dashboards);
 router.use('/general', general);
 router.use('/', root);
-
+// export router
 module.exports = router;
